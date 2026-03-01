5c73133e5c2fd430cb6626c6c38f560c61db55f1
coelho26101009-source <coelho26101009@gmail.com>
2026-03-01 21:16:32 +0000

    Add files via upload

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore"; // <-- IMPORTÁMOS O FIRESTORE
 
-// As chaves oficiais do teu cofre H.E.L.I.O.S.
+// A tua chave secreta continua segura no import.meta.env
 const firebaseConfig = {
-  apiKey: "AIzaSyAg8iYdWi2bocXxVaKAvhOR-xFvOuF1Y3Y",
-  authDomain: "helios-6ee68.firebaseapp.com",
-  projectId: "helios-6ee68",
-  storageBucket: "helios-6ee68.firebasestorage.app",
-  messagingSenderId: "664370308050",
-  appId: "1:664370308050:web:220ca26023c58f20bdde0b"
+  apiKey: (import.meta as any).env.VITE_FIREBASE_API_KEY,
+  authDomain: (import.meta as any).env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: (import.meta as any).env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: (import.meta as any).env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: (import.meta as any).env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: (import.meta as any).env.VITE_FIREBASE_APP_ID
 };
 
-// Iniciar a aplicação Firebase
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exportar as ferramentas que vamos usar no App.tsx
+// Inicializa a Autenticação (O Login)
 export const auth = getAuth(app);
-export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
+
+// Inicializa a Base de Dados (A Memória)
+export const db = getFirestore(app);
