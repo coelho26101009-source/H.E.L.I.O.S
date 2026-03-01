5a8cbcc985a79cb9c0d1350c321d09226df48295
coelho26101009-source <coelho26101009@gmail.com>
2026-03-01 22:21:50 +0000

    Add files via upload

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Aqui escrevemos as chaves diretamente (é seguro para o Firebase frontend!)
 const firebaseConfig = {
-  apiKey: "AIzaSyAgBiYdWi2bocXxVaKavhQR-xFv0uF1Y3Y",
+  apiKey: "AIzaSyAg8iYdWi2bocXxVaKAvhOR-xFvOuF1Y3Y",
   authDomain: "helios-6ee68.firebaseapp.com",
   projectId: "helios-6ee68",
   storageBucket: "helios-6ee68.firebasestorage.app",
   messagingSenderId: "664370308050",
-  appId: "1:664370308050:web:220ca26023c58f20bdde0b"
+  appId: "1:664370308050:web:220ca26023c58f20bdde0b",
+  measurementId: "G-06594GD034"
 };
 
 // Inicializa o Firebase
